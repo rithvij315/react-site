99186663f25b5d4363ad86891bd20a7332daf763
Rithvij <rithvij315@gmail.com>
2023-08-29 10:50:42 -0700

    updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
         <div className="navbar">
             <img src={logo} className="react-logo" />
             <h3>ReactFacts</h3>
-            <h4>React Course - Project 1</h4>
+            <h4>My React Site</h4>
         </div>
     )
 }
